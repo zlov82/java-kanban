75dca8ae20a3fb9fedf3a9473e048cc263218bc6
Andrey Kozlov <zlov82@yandex.ru>
2024-04-03 15:22:09 +0300

    feat: изменен принцип хранения истории

--- src/ru/yandex/javacourse/kanban/manager/InMemoryHistoryManager.java
+++ src/ru/yandex/javacourse/kanban/manager/InMemoryHistoryManager.java
@@ -3,15 +3,20 @@ package ru.yandex.javacourse.kanban.manager;
 import ru.yandex.javacourse.kanban.tasks.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private static final int MAX_HISTORY_COUNT = 10;
-    private final List<Task> historyDb;
+    private final Map<Integer, Node<Task>> historyDb;
+    private Node<Task> head;
+    private Node<Task> tail;
 
     public InMemoryHistoryManager() {
-        this.historyDb = new ArrayList<>();
+        this.historyDb = new HashMap<>();
+        this.head = null;
+        this.tail = null;
     }
 
     @Override
@@ -19,15 +24,78 @@ public class InMemoryHistoryManager implements HistoryManager {
         if (task == null) {
             return;
         }
-        if (historyDb.size() == MAX_HISTORY_COUNT) {
-            historyDb.remove(0);
-        }
-        historyDb.add(task);
+        int id = task.getId();
+        remove(id); //удаляем из мапы и пересобираем связи между нодами
+        historyDb.put(id, linkLast(task));
+
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(historyDb);
+        return getTasks();
     }
 
+    @Override
+    public void remove(int id) {
+        Node<Task> removedNode = historyDb.remove(id);
+
+        if (removedNode != null) {
+            removeNode(removedNode);
+        }
+
+    }
+
+    private void removeNode(Node<Task> node) {
+        final Node<Task> prevNode = node.prev;
+        final Node<Task> nextNode = node.next;
+
+        // Изменяем ссылки внутри списка
+        if (prevNode != null) {
+            prevNode.next = nextNode;
+        }
+        if (nextNode != null) {
+            nextNode.prev = prevNode;
+        }
+
+        if (head == node) { // удаляемая нода является головой
+            head = nextNode;
+        }
+        if (tail == node) { // удяляемая нода - хвост
+            tail = prevNode;
+        }
+
+    }
+
+    private Node<Task> linkLast(Task task) {
+        final Node<Task> oldTail = tail;
+        final Node<Task> oldHead = head;
+        final Node<Task> newNode;
+
+        if (oldTail != null) {
+            newNode = new Node<>(oldTail, task, null);
+            tail = newNode;
+            oldTail.next = newNode;
+        }else {
+            newNode = new Node<>(null, task, oldHead);
+            head = newNode;
+            if (oldHead == null) {
+                tail = newNode;
+            } else {
+                oldHead.prev = newNode;
+            }
+        }
+        return newNode;
+    }
+
+    private List<Task> getTasks(){
+        Node<Task> node = head;
+        List<Task> historyList = new ArrayList<>();
+
+        for (int i = 0; i < historyDb.size(); i++) {
+            historyList.add(node.data);
+            node = node.next;
+        }
+
+        return  historyList;
+    }
 }
